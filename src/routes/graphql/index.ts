ee9914b885290bbdc17c4bdd010d80fa9ff446ab
Volha Patsiahevich <zaprudskaya@gmail.com>
2024-11-03 14:29:39 +0100

    feat: resolve rule-test problem

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,12 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, GraphQLError, parse, validate, ValidationRule } from 'graphql';
 import schema from './mySchema.js';
+import depthLimit from 'graphql-depth-limit';
+
+interface DepthLimitRule extends ValidationRule {
+  name: string;
+}
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
@@ -16,6 +21,16 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const depthLimitValidationRule = depthLimit(5) as DepthLimitRule;
+      const validationErrors = validate(schema, parse(req.body.query), [
+        depthLimitValidationRule,
+      ]);
+      const errors: GraphQLError[] = Array.from(validationErrors);
+
+      if (errors.length > 0) {
+        return { errors };
+      }
+
       return graphql({
         schema,
         source: req.body.query,
